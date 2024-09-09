01a420ad8b0f05ca2aa65ea7ac69c6618c06198f
leo-jnesis <ltrouillet@jnesis.com>
2024-09-09 16:09:17 +0200

    added capacitor android + ios

---
+++ android/app/src/main/java/com/jnesis/rncap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jnesis.rncap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jnesis.rncap',
   appName: 'rncap',
   webDir: 'www'
 };
